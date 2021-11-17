15dc172bc7c0ef243d1f63df8a0a6e62384ada94
TeeVT <93247900+TeeVT@users.noreply.github.com>
2021-11-17 09:49:44 +0200

    passport

--- app.js
+++ app.js
@@ -2,20 +2,29 @@
 const express = require('express');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
+const passport = require('./utils/pass');
 const app = express();
 const port = 3000;
-const username = 'foo';
-const password = 'bar';
+
+
+const loggedIn = (req, res, next) => {
+  if (req.user) {
+    next();
+  } else {
+    res.redirect('/form');
+  }
+};
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-
 app.use(cookieParser());
-app.use(session({
-    secret: 'weewqr',
-    cookie: { maxAge: 60000 }
+app.use(session({ secret: 'weewqr',
+cookie: { maxAge: 60000 },
+resave: false,
+saveUninitialized: true, 
 }));
-
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.set('views', './views');
 app.set('view engine', 'pug');
@@ -42,27 +51,18 @@ app.get('/form', (req, res) => {
   res.render('form');
 });
 
-app.get('/secret', (req, res) => {
-  if(req.session.logged) {
-    res.render('secret');
-  } else {
-  res.redirect('/form');
-}
+app.get('/secret', loggedIn, (req, res) => {
+  res.render('secret');
 });
 
-app.post('/login', (req, res ) => {
-  console.log(req.body);
-  if(req.body.password === password && req.body.username === username){
-    req.session.logged = true;
-    res.redirect('/secret');
-  } else {
-    req.session.logged = false;
-    res.redirect('/form');
-  }
+app.post('/login', passport.authenticate('local', {failureRedirect: '/form'}),
+(req, res) => {
+  console.log('success');
+  res.redirect('/secret');
 });
 
 app.get('/logout', (req, res) => {
-  req.session.destroy();
+  req.logout();
   res.redirect('/');
 });
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
